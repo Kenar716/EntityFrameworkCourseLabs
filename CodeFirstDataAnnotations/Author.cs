3adb1e91543d0ff406d86f04630d4320aa9c108a
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2018-10-20 14:26:08 -0500

    Add initial project for exercices for Code First Data Annotations.

---
+++ CodeFirstDataAnnotations/Author.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDataAnnotations
+{
+    public class Author
+    {
+        public Author()
+        {
+            Courses = new HashSet<Course>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
