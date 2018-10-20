2cc826238554d234f8d70eefeadeba429ab30a44
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2018-10-20 16:59:29 -0500

    Organizing Config FluentAPI
    Organizing Configutration of Fluent API for a better maintenance of code moving the configuration to separate folder and class using the EntityTypeConfiguration call them from the DbContext with the method OnModelCreating.

---
+++ FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            //Table Overwrites
+            ToTable("tbl_Course");
+
+            //Primary Keys Overwrites
+            HasKey(c => c.Id);
+
+            //Properties Configuration (sort alphabetically)
+            Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            //RelationsShips
+            HasRequired(c => c.Author) //Navigation Property
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course); //This property belons to the Model-Table that is the parent in the relationship
+
+            HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m =>
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+        }
+    }
+}
