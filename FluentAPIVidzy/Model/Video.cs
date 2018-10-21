6a9909c7a7018bdea4e072848421097c2c72ee92
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2018-10-21 02:23:48 -0500

    Add FluentAPIVidzy project
    The exercises of section 5 take resumes with the project CodeFirstVidzy.
    FluentAPIVidzy is a copy of  that project whit a rename in the namespaces.

---
+++ FluentAPIVidzy/Model/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPIVidzy.Model
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public Classification ClassificationType { get; set; }
+
+        public enum Classification : byte
+        {
+            Silver = 1,
+            Gold = 2,
+            Platinum = 3
+        }
+    }
+}
