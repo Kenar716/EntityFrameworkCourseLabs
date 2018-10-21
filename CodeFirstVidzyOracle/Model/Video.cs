4adfa03759178b6045eddaeb3001c08010f59c0d
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2018-10-21 01:14:30 -0500

    2nd Iteration CodeFirstOracle
    The relationship between Videos and Genres change from One-to-Many to One-to-One.
    This migration delete the VideoGenres table and add a column GenreId in Videos.
    
    In the exercise with SQL Server substitute the property IList<Genre> Genres in the class Video with single Genre was enough for understand the relationship change.
    
    In Oracle it was necesary add a property class GenreId, because without this property EF was renaming the table VideoGenres to Video.

--- CodeFirstVidzyOracle/Model/Video.cs
+++ CodeFirstVidzyOracle/Model/Video.cs
@@ -11,6 +11,7 @@ namespace CodeFirstVidzyOracle.Model
         public int  Id { get; set; }
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public IList<Genre> Genres { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
     }
 }
