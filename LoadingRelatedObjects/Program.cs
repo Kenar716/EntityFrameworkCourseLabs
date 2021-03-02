4d5c6685349acfb8ab3355801f891a788364de82
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2021-03-01 23:11:54 -0600

    The project '09.- LoadingRelatedObjectt' is added, this project is a copy from '08.- DataLINQ'.

---
+++ LoadingRelatedObjects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proceso iniciado.");
+            try
+            {
+                var dbContext = new PlutoContext();
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            Console.WriteLine("Proceso terminado.");
+            Console.ReadLine();
+        }
+    }
+}
